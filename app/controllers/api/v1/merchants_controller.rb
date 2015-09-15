785bb3c28c75869fd00d0937b41417878904c66e
Samson Brock <SamsonLB@gmail.com>
2015-09-15 15:12:36 -0600

    Refactor find, find_all, random into application controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,16 +1,4 @@
 class Api::V1::MerchantsController < ApplicationController
-  def find
-    respond_with json: Merchant.find_like_by(attribute, params[attribute])
-  end
-
-  def find_all
-    respond_with json: Merchant.find_all_like_by(attribute, params[attribute])
-  end
-
-  def random
-    respond_with json: Merchant.random
-  end
-
   def show
     respond_with json: Merchant.find_by(id: params[:id])
   end
