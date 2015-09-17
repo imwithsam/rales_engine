96b7c91291dcf66723d0b38fef7bb1de95f813a3
Samson Brock <SamsonLB@gmail.com>
2015-09-17 07:20:19 -0600

    Add customers controller tests

--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -1,6 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CustomersController, type: :controller do
+  describe "#index" do
+    it "returns all customers" do
+      Customer.create(first_name: "Jane", last_name: "Doe")
+      Customer.create(first_name: "John", last_name: "Doe")
+      Customer.create(first_name: "Sam", last_name: "Brock")
+
+      get :index, format: :json
+      customers = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(customers.count).to eq(3)
+    end
+  end
+
+  describe "#show" do
+    it "returns a customer" do
+      customer_1 = Customer.create(first_name: "Jane", last_name: "Doe")
+      Customer.create(first_name: "John", last_name: "Doe")
+      Customer.create(first_name: "Sam", last_name: "Brock")
+
+      get :show, format: :json, id: customer_1.id
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(customer[:first_name]).to eq(customer_1.first_name)
+    end
+  end
+
+  describe "#find" do
+    it "finds a customer by first name" do
+      customer_1 = Customer.create(first_name: "Jane", last_name: "Doe")
+      Customer.create(first_name: "John", last_name: "Doe")
+      Customer.create(first_name: "Sam", last_name: "Brock")
+
+      get :find, format: :json, first_name: customer_1.first_name
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(customer[:first_name]).to eq(customer_1.first_name)
+    end
+  end
+
+  describe "#find_all" do
+    it "finds all customers by last name" do
+      customer_1 = Customer.create(first_name: "Jane", last_name: "Doe")
+      Customer.create(first_name: "John", last_name: "Doe")
+      Customer.create(first_name: "Sam", last_name: "Brock")
+
+      get :find_all, format: :json, last_name: customer_1.last_name
+      customers = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(customers.count).to eq(2)
+    end
+  end
+
+  describe "#random" do
+    it "returns a random customer" do
+      customer_1 = Customer.create(first_name: "Jane", last_name: "Doe")
+      customer_2 = Customer.create(first_name: "John", last_name: "Doe")
+      customer_3 = Customer.create(first_name: "Sam", last_name: "Brock")
+
+      get :random, format: :json
+      customer = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(customer[:first_name]).to satisfy do |first_name|
+        [customer_1.first_name,
+         customer_2.first_name,
+         customer_3.first_name].include?(first_name)
+      end
+    end
+  end
+
   describe "#invoices" do
     it "returns a collection of associated invoices" do
       customer = Customer.create(first_name: "Jane", last_name: "Doe")
